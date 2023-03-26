b65ad7893fe20d6f011c09eadfa2adab8c7347e7
Wojciech Rzepka <wojciech.rzepka@skillandchill.com>
2023-03-26 14:50:40 +0200

    post-reorganize

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
     },
     'import/resolver': {
       node: {
-        paths: ['src'],
+        paths: ['componenets'],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
