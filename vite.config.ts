5a5b6aed9cf63c5903309baffe6a8cd55de59d31
Wojciech Rzepka <wojciech.rzepka@skillandchill.com>
2023-03-26 11:39:58 +0200

    submodule reorganize

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
-import {defineConfig} from "vite"
-import {resolve} from "path"
-import react from "@vitejs/plugin-react"
-import dts from "vite-plugin-dts"
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts()],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/components/index.ts"),
-      name: "black-wave",
+      entry: resolve(__dirname, './components/index.ts'),
+      name: 'black-wave',
       // the proper extensions will be added
-      fileName: "black-wave",
+      fileName: 'black-wave',
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
   },
-})
+});
